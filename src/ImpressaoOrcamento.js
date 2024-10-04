721b4f68bedaab25f881ef8c1cef4bd1a129ffa7
Doug Corbellini <dougcorbellini@Mac-mini-de-Doug.local>
2024-10-03 21:44:24 -0300

    Descrição das alterações

--- src/ImpressaoOrcamento.js
+++ src/ImpressaoOrcamento.js
@@ -21,23 +21,23 @@ const ImpressaoOrcamento = ({
       </div>
 
       <div style={{ marginBottom: '40px' }}>
-        <img src="/imagens/cabecalho.jpg" alt="Cabeçalho" style={{ width: '100%', height: 'auto' }} />
+        <img src={process.env.PUBLIC_URL + '/imagens/cabecalho.jpg'} alt="Cabeçalho" style={{ width: '100%', height: 'auto' }} />
       </div>
 
       {temServicosEspeciais && (
         <>
-          <h3>{numeroSecao++}. Metodologia de Trabalho</h3>
+          <h3>Metodologia de Trabalho</h3>
           <div style={{ marginTop: '30px' }}>
             <h3>{numeroSecao++}. Atendimento / Briefing</h3>
-            <img src="/imagens/imagem1.jpg" alt="Imagem Atendimento / Briefing" style={{ width: '100%', height: 'auto', marginTop: '10px', marginBottom: '10px' }} />
+            <img src={process.env.PUBLIC_URL + '/imagens/imagem1.jpg'} alt="Imagem Atendimento / Briefing" style={{ width: '100%', height: 'auto', marginTop: '10px', marginBottom: '10px' }} />
             <p>Feito o primeiro contato com o cliente, é marcada uma conversa onde colhemos todos os dados e informações que possam ser úteis ao desenvolvimento do projeto. Histórico da empresa, pessoa física, características, atuação e informações adicionais. Todo esse apanhado de informações formam o que chamamos de briefing, que vai dar ao designer o rumo a ser tomado para o desenvolvimento do projeto.</p>
             
             <h3>{numeroSecao++}. Projeto</h3>
-            <img src="/imagens/imagem2.jpg" alt="Imagem Projeto" style={{ width: '100%', height: 'auto', marginTop: '10px', marginBottom: '10px' }} />
+            <img src={process.env.PUBLIC_URL + '/imagens/imagem2.jpg'} alt="Imagem Projeto" style={{ width: '100%', height: 'auto', marginTop: '10px', marginBottom: '10px' }} />
             <p>Aprovada a proposta comercial, inicia-se o projeto. Nesta etapa são realizadas pesquisas e análises diversas (estratégias de mercado, de concorrência, estudos de tendências, de formas, cores e tipografias). Tendo essas pesquisas em mãos, bem como as informações do item 1, é definido o CONCEITO, a alma do projeto. O conceito é o que produto vai expressar, o que ele vai nos dizer através de seu desenho, sua face. Usufruindo de todas essas informações, o desenvolvimento do projeto vai ser executado. Obedecendo as etapas estabelecidas na proposta de trabalho. Logo que o projeto estiver concretizado são apresentados ao cliente o layout do trabalho em questão. Nesta etapa, cliente e designer discutem sobre o projeto até chegarem ao resultado mais satisfatório possível. Então o projeto é aprovado e finalizado, deixando-o apto à implantação.</p>
             
             <h3>{numeroSecao++}. Implantação</h3>
-            <img src="/imagens/imagem3.jpg" alt="Imagem Implantação" style={{ width: '100%', height: 'auto', marginTop: '10px', marginBottom: '10px' }} />
+            <img src={process.env.PUBLIC_URL + '/imagens/imagem3.jpg'} alt="Imagem Implantação" style={{ width: '100%', height: 'auto', marginTop: '10px', marginBottom: '10px' }} />
             <p>Nesta etapa é apresentado orçamentos de empresas terceiras, os quais são analisados definindo o fornecedor para implantação do projeto (impressão, adesivagem, serigrafia, etc). O designer acompanha a produção do mesmo, conferindo qualidade máxima nas questões que o envolvem (materiais, cores, acabamentos, entre outros), até a entrega ao cliente.</p>
           </div>
         </>
